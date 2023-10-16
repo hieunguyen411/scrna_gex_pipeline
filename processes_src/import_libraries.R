0bc604adc8c20a063c8205cfb5a8c0f69be614f6
trnguyenuka <trnguyenuka@github.com>
2023-10-16 07:28:19 +0200

    update 10.2023

--- processes_src/import_libraries.R
+++ processes_src/import_libraries.R
@@ -43,8 +43,16 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 new.bioc.packages <- bioc.packages[!(bioc.packages %in% installed.packages()[,"Package"])]
 
 # Install new packages #########################################################
+
+print("Installing new packages by install.packages...")
 if(length(new.packages)) install.packages(new.packages)
+# if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+print("Finish installing new packages by install.packages")
+print("Start installing packages from Bioconductor...")
+
 if(length(new.bioc.packages)) BiocManager::install(new.bioc.packages, update = FALSE, ask = TRUE)
+print("Finish installing packages from Bioconductor")
+
 
 # Import all packages ##########################################################
 package_loading_Status <- lapply(list.of.packages, 
